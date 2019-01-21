a9646c5721cb1d13272ccdeedb437b19eb6fa7f9
zampierimatteo91 <zampierimatteo.91@gmail.com>
2019-01-21 03:39:35 +0100

    upd

--- c++/scripts/plot.sh
+++ c++/scripts/plot.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 set terminal png size 1200,600
-set output "search.png"
+set output "../plots/search.png"
 set xlabel 'Number of Keys'
 set ylabel 'Time (us)'
 
 set key top center
-
-plot 'data.txt' using 1:2 w lp lw 2 lc rgb 'red' title 'Non-balanced tree',\
-		'data.txt' using 1:3 w lp lw 2 lc rgb 'blue' title 'Self-balanced tree',\
-		'data.txt' using 1:4 w lp lw 2 lc rgb 'green' title 'std::map'
+set grid 
+plot '../data/data.txt' using 1:2 w lp lw 2 lc rgb 'red' title 'Non-balanced tree',\
+		'../data/data.txt' using 1:3 w lp lw 2 lc rgb 'blue' title 'Self-balanced tree',\
+		'../data/data.txt' using 1:4 w lp lw 2 lc rgb 'green' title 'std::map'
 
 replot
+
 
 set output "figure.png"
 set xlabel 'Number of keys'
@@ -23,4 +24,4 @@ set style data histogram
 set style histogram cluster gap 1
 set style fill solid border -1
 set boxwidth 0.7
-plot 'data.txt' using (($2-$3)/$3):xtic(1) tit 'unbalanced', '' u (($4-$3)/$3) tit 'std::map'
+plot '../data/data.txt' using (($2-$3)/$3):xtic(1) tit 'unbalanced', '' u (($4-$3)/$3) tit 'std::map'
